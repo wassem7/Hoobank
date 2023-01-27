e74bec0f57b13ba920d0df656ca6db7d18c51f97
Wassem Darkwa <waseemdarkwa@gmail.com>
2023-01-27 07:59:57 +0000

    Clients section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
-
-const Clients = () => {
-  return <div>Clients</div>;
-};
+import { clients } from '../constants';
+import styles from '../style';
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div
+          key={client.id}
+          className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] w-[100px] object-contain`}
+        >
+          <img
+            src={client.logo}
+            alt='client-logo'
+            className='sm:w-[192px] w-[100px] object-contain  hover:scale-105 hover:duration-200 hover:bg-clip-text '
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
 export default Clients;
